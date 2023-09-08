65c25f7ad461a2e7cd9a6ddda1ed2ddb25e3b665
Kaan Koç <kock@mef.edu.tr>
2023-09-08 16:54:34 +0300

    first commit

---
+++ programs/solonaexample/src/lib.rs
@@ -0,0 +1,61 @@
+use anchor_lang::prelude::*;
+
+
+declare_id!("7EdZPAMRSj6XQV2t8tm2KGBJoy9NZ9Twrky8L9KEqezG");
+
+#[program]
+pub mod solonaexample {
+    use super::*;
+
+    pub fn create(ctx: Context<Initialize>) -> Result<()> {
+    
+        // Placeholder for a function or expression that returns a Result.
+        let result = some_function_that_returns_result(ctx);
+    
+        match result {
+            Ok(_) => {
+                println!("Hello World");
+                Ok(())
+            },
+            Err(err) => {
+                println!("Error: {}", err);
+                Err(err)
+            }
+        }
+    }    
+    pub fn some_function_that_returns_result(_ctx: Context<Initialize>) -> Result<()> {
+        // print context here
+        
+        // Some logic here that returns Ok or Err.
+        Ok(())
+    }
+    
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
---
+++ tests/solonaexample.ts
@@ -0,0 +1,16 @@
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Solonaexample } from '../target/types/solonaexample';
+
+describe('solonaexample', () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Solonaexample as Program<Solonaexample>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log('Your transaction signature', tx);
+  });
+});
